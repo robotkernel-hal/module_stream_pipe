5d9f7f4af082d497d3e69f68fce3b86f4c262d5e
Robert Burger <robert.burger@dlr.de>
2019-12-17 14:38:30 +0100

    switched to conan template

--- conanfile.py
+++ conanfile.py
@@ -8,32 +8,3 @@ class MainProject(base.RobotkernelConanFile):
     exports_sources = ["*", "!.gitignore"] + ["!%s" % x for x in tools.Git().excluded_files()]
     requires = "robotkernel/[~=5]@robotkernel/stable"
 
-    def source(self):
-        filedata = None
-        filename = "project.properties"
-        with open(filename, 'r') as f:
-            filedata = f.read()
-        with open(filename, 'w') as f:
-            f.write(re.sub("VERSION *=.*[^\n]", f"VERSION = {self.version}", filedata))
-
-    def build(self):
-        self.run("autoreconf -if")
-        autotools = AutoToolsBuildEnvironment(self)
-        autotools.libs=[]
-        autotools.include_paths=[]
-        autotools.library_paths=[]
-        if self.settings.build_type == "Debug":
-            autotools.flags = ["-O0", "-g"]
-        else:
-            autotools.flags = ["-O3"]
-        autotools.configure(configure_dir=".")
-        autotools.make()
-
-    def package(self):
-        autotools = AutoToolsBuildEnvironment(self)
-        autotools.install()
-
-    def package_info(self):
-        self.cpp_info.includedirs = ['include']
-        self.cpp_info.bindirs = ['bin']
-        self.cpp_info.resdirs = ['share']
