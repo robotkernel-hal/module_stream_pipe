d97d4eaa47bcf1ed459efa26d4cb4de9195a3c05
Robert Burger <robert.burger@dlr.de>
2025-06-06 06:47:50 +0200

    change: build against robotkernel 6

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,11 @@
-from conans import tools, python_requires
+from conan import ConanFile
 
-base = python_requires("conan_template/[~=5]@robotkernel/stable")
+class MainProject(ConanFile):
+    python_requires = "conan_template/[^5.0.6]@robotkernel/stable"
+    python_requires_extend = "conan_template.RobotkernelConanFile"
 
-class MainProject(base.RobotkernelConanFile):
     name = "module_stream_pipe"
-    description = "robotkernel-5 is a modular, easy configurable hardware abstraction framework"
-    exports_sources = ["*", "!.gitignore"] + ["!%s" % x for x in tools.Git().excluded_files()]
-    requires = "robotkernel/[~=5]@robotkernel/stable"
+    description = ""
+    exports_sources = ["*", "!.gitignore"] 
+    requires = "robotkernel/[~6]@robotkernel/unstable"
 
